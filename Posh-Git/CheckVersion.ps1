dce9b7cc4ea8507adef400a2d9209131de19d110
Tim G. Thomas <tim@timgthomas.com>
2011-09-15 14:09:21 -0500

    Replaced Posh-Git and Posh-Hg with submodules

--- Posh-Git/CheckVersion.ps1
+++
@@ -1,7 +0,0 @@
-$version = git --version 2> $null
-if($version -notlike 'git version 1.7.*.msysgit.*') {
-    Write-Warning "posh-git requires msysgit version 1.7. You have $version."
-    $false
-} else {
-    $true
-}
